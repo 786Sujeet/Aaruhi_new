1f04838668a3461765161642a88e4481e4fcbe4f
Sujeet Yadav <j.sujeet786@gmail.com>
2025-04-03 20:09:28 +0530

    new changes by sujeet

---
+++ src/main/java/com/sujeet/screenshot/ScreenShot/App.java
@@ -0,0 +1,13 @@
+package com.sujeet.screenshot.ScreenShot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/sujeet/screenshot/HandlingScreenShot.java
@@ -0,0 +1,28 @@
+package com.sujeet.screenshot;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+
+	public class HandlingScreenShot {
+
+		public static void main(String[] args) throws IOException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+		TakesScreenshot screen=(TakesScreenshot) driver;
+		File src=screen.getScreenshotAs(OutputType.FILE);
+		File dest=new File("C:\\Sujeet Yadav\\ScreenShot\\TestAuto2.jpeg");
+		FileHandler.copy(src, dest);
+
+		}
+
+
+}
