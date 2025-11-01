1aa8d333a31d5661f196e0e644d32e67dc84fe42
Sujeet Yadav <j.sujeet786@gmail.com>
2025-11-02 00:10:47 +0530

    sujeet added new file

---
+++ src/test/java/com/sujeet/screenshot/Sujeet.java
@@ -0,0 +1,11 @@
+package com.sujeet.screenshot;
+
+public class Sujeet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sujeeet yadav");
+
+	}
+
+}
